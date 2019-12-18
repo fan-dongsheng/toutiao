ae4aad1a7d730a8018aa389c3ac5641bf65c7a7a
Your Name <f137000360@qq.com>
2019-12-18 17:18:55 +0800

    黑马头条-初始化项目-element-ui 注册

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUi from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 
 Vue.config.productionTip = false
+Vue.use(ElementUi)
 
 new Vue({
   router,
