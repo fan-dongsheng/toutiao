9e02e4712df522aa30f48e6d9a80f86c6a233a3f
Your Name <f137000360@qq.com>
2019-12-18 19:39:23 +0800

    黑马头条PC-初始化项目-页面分析-新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/home'
+import Login from '../views/login' // 引入组件
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-
     path: '/',
-    name: 'home',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
     component: Home
-
   }
   // {
   //   path: '/about',
---
+++ src/views/login/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>dengluye</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
