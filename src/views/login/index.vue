1e9835705a9f4afe6e314659025bd13570e14104
Your Name <f137000360@qq.com>
2019-12-18 23:32:20 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,21 +5,21 @@
             <img src="../../assets/img/logo_index.png" alt="">
         </div>
 
-        <el-form>
+        <el-form ref="myForm" :model="loginForm" :rules="loginRules">
 
-            <el-form-item style="margin-top:20px;">
-                <el-input placeholder="请输入手机号"></el-input>
+            <el-form-item style="margin-top:20px;" prop="mobile">
+                <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
             </el-form-item>
 
-            <el-form-item >
-                <el-input style="width:60%" placeholder="请输入验证码"></el-input>
-                <el-button plain style="float:right">发送验证码</el-button>
+            <el-form-item prop="code">
+                <el-input v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
+                <el-button  plain style="float:right">发送验证码</el-button>
             </el-form-item>
-             <el-form-item >
-            <el-checkbox>我已阅读并同意用户协议和条款</el-checkbox>
+             <el-form-item prop="check">
+            <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和条款</el-checkbox>
             </el-form-item >
             <el-form-item >
-            <el-button type="primary" style="width:100%;">登录</el-button>
+            <el-button @click="submitLogin" type="primary" style="width:100%;">登录</el-button>
             </el-form-item>
         </el-form>
     </el-card>
@@ -29,6 +29,57 @@
 
 <script>
 export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{
+          // 第一个规则是必须输入手机号
+          required: true, message: '请输入手机号'
+        },
+        {
+          // 第二个规则是正则表达式,判断手机号
+          pattern: /^1[3456789]\d{9}$/, message: '手机号格式不正确'
+
+        }],
+        code: [{
+          required: true, message: '请输入验证码'
+        },
+        {
+          pattern: /^\d{6}$/, message: '验证码格式不正确'
+        }
+        ],
+        check: [{
+          validator: function (rule, value, callback) {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('必须勾选'))
+            }
+          }
+
+        }]
+
+      }
+    }
+  },
+  methods: {
+    // 手动校验,需要在e-form添加ref属性,然后添加方法
+    submitLogin () {
+      // 获取e-form 实例,validate是一个函数,有两个参数
+      this.$refs.myForm.validate(function (isok) {
+        if (isok) {
+          console.log('前端校验成功,准备调用接口,传到后端')
+        } else {
+
+        }
+      })
+    }
+  }
 
 }
 </script>
