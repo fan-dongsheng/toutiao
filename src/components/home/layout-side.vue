86e915d693ac7d9361f7edea7d1abe5060e50982
Your Name <f137000360@qq.com>
2019-12-19 20:37:28 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/layout-side.vue
@@ -0,0 +1,40 @@
+<template>
+<div>
+      <!-- 上部图标 -->
+            <div class="home-aside-title">
+                <img src="../../assets/img/logo_admin.png" alt="">
+            </div>
+            <!-- 这是导航 -->
+            <el-menu background-color= "#323745" text-color="#adafb5"  active-text-color="#ffd04b" style="width:231px">
+                <el-menu-item index="1" >首页</el-menu-item>
+                    <el-submenu index="2">
+                            <template slot="title">内容管理</template>
+                            <el-menu-item index="2-1">发布文章</el-menu-item>
+                            <el-menu-item index="2-2">内容列表</el-menu-item>
+                            <el-menu-item index="2-3">评论列表</el-menu-item>
+                            <el-menu-item index="2-3">素材管理</el-menu-item>
+
+                    </el-submenu>
+                     <el-submenu index="3">
+                            <template slot="title">粉丝管理</template>
+                            <el-menu-item index="2-1">图文数据</el-menu-item>
+                            <el-menu-item index="2-2">粉丝概况</el-menu-item>
+                            <el-menu-item index="2-3">粉丝画像</el-menu-item>
+                            <el-menu-item index="2-3">粉丝列表</el-menu-item>
+
+                    </el-submenu>
+                    <el-menu-item index="4" >账户信息</el-menu-item>
+
+            </el-menu>
+            </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,8 +1,12 @@
 <template>
 <!-- //页面布局,分为左右两部分,页面再分为上下两部分 -->
 <el-container class="home-container">
-    <el-aside width="230px">Aside</el-aside>
-    <!-- //这个contaiter是main容器,分为上下两部分 -->
+    <!-- 侧边栏部分 -->
+    <el-aside  class="home-aside" width="230px">
+      <layout-side/>
+
+    </el-aside>
+    <!-- 主内容部分,这个contaiter是main容器,分为上下两部分 -->
     <el-container>
     <el-header>Header</el-header>
     <el-main>Main</el-main>
@@ -13,7 +17,11 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-side'
 export default {
+  components: {
+    'layout-side': layoutAside
+  }
 
 }
 </script>
@@ -21,6 +29,19 @@ export default {
 <style lang="less">
         .home-container{
             height: 100vh;
+            .home-aside{
+                background-color: #323745;
+                .home-aside-title{
+                    text-align: center;
+                    padding: 10px 0;
+                    background-color: #2e2f32;
+                    img{
+                        height: 30px;
+
+                    }
+                }
+            }
+
         }
 
 </style>
