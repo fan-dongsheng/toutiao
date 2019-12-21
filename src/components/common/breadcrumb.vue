ef1853d4b33798f4b9937de0f79cb465634776ae
Your Name <f137000360@qq.com>
2019-12-21 20:56:29 +0800

    黑马头条PC-面包屑组件封装

---
+++ src/components/common/breadcrumb.vue
@@ -0,0 +1,23 @@
+<template>
+<!-- 面包屑组件封装 -->
+  <el-breadcrumb >
+  <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+  <el-breadcrumb-item>
+      <!-- //数据是动态的,加一个插槽 -->
+      <slot name="title"> </slot>
+
+  </el-breadcrumb-item>
+
+</el-breadcrumb>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,8 @@
 import layoutAside from './home/layout-side'
 // 引入main内容的header
 import layoutHeader from './home/layout-header'
+// 引入面包屑组件
+import breadCrumb from './common/breadcrumb.vue'
 
 export default {
   // 这是全局注册组件的方法,调用element中的一个install方法
@@ -10,5 +12,6 @@ export default {
     // 拿到的vue对象可以注册全局组件
     Vue.component('layout-side', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb', breadCrumb)
   }
 }
