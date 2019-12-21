605159c91963009a1fe69cbc1a2747701b53c36c
Your Name <f137000360@qq.com>
2019-12-21 23:11:33 +0800

    黑马头条PC-评论列表-页面结构-table

--- src/components/common/breadcrumb.vue
+++ src/components/common/breadcrumb.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- 面包屑组件封装 -->
-  <el-breadcrumb >
+  <el-breadcrumb separator=">">
   <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
   <el-breadcrumb-item>
       <!-- //数据是动态的,加一个插槽 -->
--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>评论列表</div>
+        <!-- 评论组件 -->
+    <el-card>
+        <bread-crumb slot="header">
+            <template slot="title">
+               评论列表
+            </template>
+        </bread-crumb>
+        <!-- 这里是一个table表格 -->
+
+        <el-table >
+            <el-table-column label="标题" width="600"></el-table-column>
+            <el-table-column label="评论状态"></el-table-column>
+            <el-table-column label="总评论数"></el-table-column>
+            <el-table-column label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作"></el-table-column>
+
+        </el-table>
+
+    </el-card>
 </template>
 
 <script>
