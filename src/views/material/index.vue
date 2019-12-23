3d79465fa1d32e048c196df4776b92fd3f64cd8a
Your Name <f137000360@qq.com>
2019-12-23 17:12:29 +0800

    黑马头条PC-素材列表-页面结构-tab

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -3,12 +3,23 @@
         <bread-crumb slot="header">
             <template slot="title">素材管理</template>
         </bread-crumb>
+           <el-tabs v-model="activeName">
+                <el-tab-pane label="全部素材" name="all"></el-tab-pane>
+                <el-tab-pane label="收藏素材" name="collect"></el-tab-pane>
+
+            </el-tabs>
 
     </el-card>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 激活选中name,c-model 双向绑定
+      activeName: 'collect'
+    }
+  }
 
 }
 </script>
