52ddbe2b2632bbf98129df6b3c8b728feca43183
Your Name <f137000360@qq.com>
2019-12-26 20:29:02 +0800

    黑马头条PC-发布文章-注册富文本编辑器并使用

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,9 @@ import layoutAside from './home/layout-side'
 import layoutHeader from './home/layout-header'
 // 引入面包屑组件
 import breadCrumb from './common/breadcrumb.vue'
+// 引入富文本
+import { quillEditor } from 'vue-quill-editor'
+import 'quill/dist/quill.snow.css' // 富文本编辑器外部引用样式  三种样式三选一引入即可
 
 export default {
   // 这是全局注册组件的方法,调用element中的一个install方法
@@ -13,5 +16,6 @@ export default {
     Vue.component('layout-side', layoutAside)
     Vue.component('layout-header', layoutHeader)
     Vue.component('bread-crumb', breadCrumb)
+    Vue.component('quill-editor', quillEditor)
   }
 }
--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -10,9 +10,9 @@
                 <el-input v-model="formData.title" placeholder="文章名称" style="width:400px"></el-input>
             </el-form-item>
              <el-form-item prop="content" label="内容" >
-                <el-input v-model="formData.content" type="textarea" ></el-input>
+                <quill-editor v-model="formData.content" type="textarea" style="height:300px;" ></quill-editor>
             </el-form-item>
-            <el-form-item prop="type" label="封面">
+            <el-form-item prop="type" label="封面" style="margin-top:100px;">
                 <el-radio-group v-model="formData.cover.type">
                     <el-radio :label="1">单图</el-radio>
                     <el-radio :label="3">三图</el-radio>
